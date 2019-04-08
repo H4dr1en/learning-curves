9e283ed79fabf50550ae84163c4cc95e9dda1b6d
H4dr1en <h4dr1en@pm.me>
2019-04-08 14:24:44 +0200

    Modified functions signatures to fit README documentation

--- learning_curve.py
+++ learning_curve.py
@@ -1,6 +1,8 @@
 # Imports
 import warnings
+import gc
 import time 
+
 from sklearn.metrics import r2_score
 import scipy.optimize as optimize
 import numpy as np
@@ -13,6 +15,7 @@ from matplotlib.offsetbox import AnchoredText
 class Predictor():
 
     def __init__(self, name:str, func:callable, guess:list, scoring:callable=r2_score):
+        """ Create a Predictor with a given {name}, {function} and initial parameters: {guess}. A custom scoring function can also be used."""
         self.name = name        
         self.func = func
         self.guess = guess
@@ -43,8 +46,8 @@ class LearningCurve():
 
     def get_lc(self, estimator, X, Y, train_sizes=None, test_size=0.2, n_splits=3, verbose=1, n_jobs=-1, **kwargs):
         """ Compute and plot the learning curve."""
-        result = self.train(estimator, X, Y, train_sizes=None, test_size=0.2, n_splits=3, verbose=verbose, n_jobs=-1, **kwargs)
-        return self.plot_lc(**result, predictor="best")
+        self.train(estimator, X, Y, train_sizes=None, test_size=0.2, n_splits=3, verbose=verbose, n_jobs=-1, **kwargs)
+        return self.plot_lc(predictor="best")
 
     def train(self, estimator, X, Y, train_sizes=None, test_size=0.2, n_splits=3, verbose=1, n_jobs=-1):
         """ Compute the learning curve of an estimator over a dataset. Returns an object that can then be passed to plot_lc function.
@@ -71,25 +74,27 @@ class LearningCurve():
             "test_scores_mean"    : np.mean(test_scores, axis=1),
             "test_scores_std"     : np.std(test_scores, axis=1),
             "time"                : time.perf_counter() - t_start
-        }           
+        }  
+
+        gc.collect()         
 
         return self.recorder["data"]
 
 
     def get_predictor(self, name:str):
-        """ This gets the first predictor with matching {name}, and returns None if no predictor matches. """
+        """ Get the first predictor with matching {name}. Returns None if no predictor matches. """
         return next((P for P in self.predictors if P.name == name), None)
 
 
     def fit_all(self, x, y):
         """ Fit a curve with all the functions and retrieve r2 score if y_pred is finite.
-            Returns an array of tuples (popt,pcov,r2)."""
+            Returns an array of predictors with the updated params and score."""
         return [self.fit(p,x,y) for p in self.predictors]
 
 
     def fit(self, predictor, x, y):
         """ Fit a curve with a predictor and retrieve r2 score if y_pred is finite.
-            Returns a tuple (popt,pcov,r2)."""
+            Returns the predictor with the updated params and score."""
 
         P = self.get_predictor(predictor) if isinstance(predictor, str) else predictor
         assert isinstance(P, Predictor), "The given Predictor is not a Predictor object (or could not be found)."
@@ -103,8 +108,16 @@ class LearningCurve():
         return P
 
 
-    def best_predictor(self, x, y):
+    def best_predictor(self):
+        """ Find the best predictor of the LearningCurve data for the test score learning curve."""
+
+        if not 'data' in self.recorder:
+            raise RuntimeError("recorder is empty. You must first compute learning curve data points using the train method.")
+        return self.best_predictor_cust(self.recorder["data"]["train_sizes"], self.recorder["data"]["test_scores_mean"])
+
+    def best_predictor_cust(self, x, y):
         """ Find the best predictor for the test score learning curve."""
+        
         first_p = self.predictors[0]
         if not all([first_p.scoring == P.scoring for P in self.predictors]):
             warnings.warn("Scoring functions should be the same accross Predictors in order to have comparable results.")
@@ -116,9 +129,18 @@ class LearningCurve():
         return P.name, P.score, P
 
 
-    def plot_lc(self, train_sizes, train_scores_mean, train_scores_std, test_scores_mean, test_scores_std, 
-                predictor:str="None", ylim=None, figsize=None, title=None, **kwargs):
-        """ Plot the training and test learning curve, and optionally a fitted function. """
+    def plot_lc(self, predictor:str=None, ylim=None, figsize=None, title=None, **kwargs):
+        """ Plot the training and test learning curve of the LearningCurve data, and optionally a fitted function. 
+            - predictor: The name of the predictor to use for fitting the learning curve. Can also be "all" or "best".
+        """
+        if not 'data' in self.recorder:
+            raise RuntimeError("recorder is empty. You must first compute learning curve data points using the train method.")
+        return self.plot_cust_lc(**self.recorder["data"], **kwargs)
+
+
+    def plot_cust_lc(self, train_sizes, train_scores_mean, train_scores_std, test_scores_mean, test_scores_std, 
+                        predictor:str=None, ylim=None, figsize=None, title=None, **kwargs):
+        """ Plot any training and test learning curve, and optionally a fitted function. """
     
         fig, ax = plt.subplots(1,1,figsize=figsize)
         if 'title' is not None:
@@ -136,7 +158,6 @@ class LearningCurve():
         ax.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
         ax.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
 
-
         predictors_to_print = []
 
         if predictor == "all":
@@ -145,7 +166,7 @@ class LearningCurve():
         elif predictor == "best":    
             self.fit_all(train_sizes, test_scores_mean)
             predictors_to_print.append(self.best_predictor(train_sizes, test_scores_mean)[2])  
-        else:
+        elif predictor is not None:
             P = self.get_predictor(predictor)
             if P is not None:
                 self.fit(P, train_sizes, test_scores_mean)
