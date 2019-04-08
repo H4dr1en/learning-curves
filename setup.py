b004958edfc1d912da9b94e7e027572af98b07cb
H4dr1en <h4dr1en@pm.me>
2019-04-08 15:00:15 +0200

    Fixed typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="learning-curve",
+    name="learning-curves",
     version="0.1.0",
     author="H4dr1en",
     author_email="h4dr1en@pm.me",
